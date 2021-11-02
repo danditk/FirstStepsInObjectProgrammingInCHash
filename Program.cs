e5f1e42dd79abc2263dec2ba1e6bf68b4f53cea3
Darek <darekt@abmicro.pl>
2021-11-02 22:11:12 +0100

    Creating first objects

--- Program.cs
+++ Program.cs
@@ -10,11 +10,33 @@ namespace FirstStepsInObjectProgrammingInCHash
     {
         static void Main(string[] args)
         {
+            Person User1 = new Person();
+            User1.name = "Phil";
+            User1.surname = "Konieczny";
+            User1.gender = "Male";
+            User1.age = 30;
+            User1.hobby = "Cryptocurentsy";
+            User1.job = "Rentier / YouTuber / Investor / FreeLancer";
+
+            Person User2 = new Person();
+            User2.name = "Maciek";
+            User2.surname = "ZKlanu";
+            User2.gender = "Male?";
+            User2.age = 38;
+            User2.hobby = "Miłość";
+            User2.job = "Aktor / Somelier / Wokalista / Multiinstrumentalista";
+
+            Console.ReadKey();
         }
 
-        class Test
+        class Person
         {
-            //TODO
+            public string name;
+            public string surname;
+            public string gender;
+            public int age;
+            public string hobby;
+            public string job;
         }
     }
 }
