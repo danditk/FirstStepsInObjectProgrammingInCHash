9bb0d6fb20b8c5123392f61a054879defab9465b
Darek <darekt@abmicro.pl>
2021-11-02 22:30:03 +0100

    Move class Person to new document.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstStepsInObjectProgrammingInCHash
+{
+    class Person
+    {
+        public string name;
+        public string surname;
+        public string gender;
+        public int age;
+        public string hobby;
+        public string job;
+
+        public void Introduction()
+        {
+            Console.WriteLine($"Mam na imie {name}, a na nazwisko {surname}.");
+            Console.WriteLine($"Moja oficialna płeć to {gender} i mam {age} lat.");
+            Console.WriteLine($"Mój zawód to {job},\na w wolnym czasie lubię spędzać czas nad: {hobby}, bo to moje hobby ;).");
+            Console.WriteLine(); Console.WriteLine();
+        }
+    }
+}
